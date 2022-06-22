471bad1b352aafe51c17c24f9055f7d0a5f8a3e3
Hernando Guzmán <A7XHernando@gmail.com>
2022-06-22 14:31:45 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  console.log('Endpoint called')
+  res.status(200).end()
+})
+
+app.listen(port, () =>
+  console.log(`⚡️[server]: Server is running on port ${port}...`)
+)
+
+module.exports = app
