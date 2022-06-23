6e3deae7589a0146b72edd0ef163ecc221be7a11
Hernando Guzmán <A7XHernando@gmail.com>
2022-06-23 17:48:22 -0300

    Add notification message on tag modification

--- index.ts
+++ index.ts
@@ -57,11 +57,49 @@ app.post('/', async (req, res) => {
       }
     )
 
+    // Push topic ID to DB
+
     res.status(200).end()
     return
   }
 
   if (eventType === 'topic_edited') {
+    const topicRes = await axios.get(DISCOURSE_URL + `/t/${body.topic.id}.json`)
+    const topicData = topicRes.data
+    const firstPostId = topicData.post_stream.posts[0].id
+
+    const changelogRes = await axios.get(
+      DISCOURSE_URL + `/posts/${firstPostId}/revisions/latest.json`
+    )
+    const changelogData = changelogRes.data
+    const { previous: previousTags, current: currentTags } =
+      changelogData.tags_changes
+    const impactTagsBefore = previousTags.some(
+      (tag: string) => tag === HIGH_IMPACT || tag === MEDIUM_IMPACT
+    )
+    const impactTagsNow = currentTags.some(
+      (tag: string) => tag === HIGH_IMPACT || tag === MEDIUM_IMPACT
+    )
+
+    if (!impactTagsBefore && impactTagsNow) {
+      await axios.post(
+        DISCOURSE_URL + '/posts.json',
+        {
+          topic_id: body.topic.id,
+          raw: 'Attention @impact-alerts this proposal has been given an Impact tag, please consider reviewing it.',
+        },
+        {
+          headers: {
+            'Api-Key': DISCOURSE_API_KEY,
+            'Api-Username': DISCOURSE_API_USERNAME,
+          },
+        }
+      )
+
+      // Push topic ID to DB
+    }
+
+    res.status(200).end()
     return
   }
 
