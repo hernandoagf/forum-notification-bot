01d2bf6aeea2376401f418e9ec4f63cbbea18a82
Hernando Guzmán <A7XHernando@gmail.com>
2022-06-23 15:33:22 -0300

    Add log

--- index.ts
+++ index.ts
@@ -11,13 +11,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  console.log(req.Headers)
-  console.log(req.headers)
-  console.log(req.body)
-  console.log(req.payload)
-  console.log(req.Payload)
-  console.log(req.data)
-  console.log(req.formData)
+  console.log(req)
   res.status(200).end()
 })
 
