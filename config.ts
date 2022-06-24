43de1b84b4ed57d36fed51b8439771b5d867f4b1
Hernando Guzmán <A7XHernando@gmail.com>
2022-06-24 15:30:40 -0300

    Add full Discourse logic

---
+++ config.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const DISCOURSE_URL = process.env.DISCOURSE_URL || ''
+const DISCOURSE_API_KEY = process.env.DISCOURSE_API_KEY || ''
+const DISCOURSE_API_USERNAME = process.env.DISCOURSE_API_USERNAME || ''
+
+export const forumAxios =
+  !DISCOURSE_URL || !DISCOURSE_API_KEY || !DISCOURSE_API_USERNAME
+    ? undefined
+    : axios.create({
+        baseURL: DISCOURSE_URL,
+        headers: {
+          'Api-Key': DISCOURSE_API_KEY,
+          'Api-Username': DISCOURSE_API_USERNAME,
+        },
+      })
--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
 import express from 'express'
 import { config } from 'dotenv'
 import { HmacSHA256 } from 'crypto-js'
-import axios from 'axios'
 
-import { HIGH_IMPACT, MEDIUM_IMPACT } from './constant'
+import { HIGH_IMPACT } from './constant'
+import { forumAxios } from './config'
 
 config()
 
 const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET || ''
-const DISCOURSE_URL = process.env.DISCOURSE_URL || ''
-const DISCOURSE_API_KEY = process.env.DISCOURSE_API_KEY || ''
 const DISCOURSE_API_USERNAME = process.env.DISCOURSE_API_USERNAME || ''
 
 const app = express()
@@ -21,96 +19,60 @@ app.post('/', async (req, res) => {
   const headers = req.headers
   const headerHash = headers['x-discourse-event-signature']
   const eventType = headers['x-discourse-event']
-
-  // Perform security verifications
-  if (!headerHash) {
-    res.status(400).end()
-    return
-  }
+  const impactType = body.topic.tags.includes(HIGH_IMPACT) ? 'High' : 'Medium'
 
   const hmac = HmacSHA256(JSON.stringify(body), WEBHOOK_SECRET)
   const hash = `sha256=${hmac}`
 
-  if (hash !== headerHash) {
-    res.status(403).end()
-    return
-  }
-
-  if (!DISCOURSE_URL || !DISCOURSE_API_KEY || !DISCOURSE_API_USERNAME) {
-    res.status(200).end()
-    return
-  }
-
+  // Perform security verifications
+  if (!headerHash) res.status(400).end()
+  else if (hash !== headerHash) res.status(403).end()
+  else if (!forumAxios) res.status(200).end()
   // Check if webhook is of interest
-  if (eventType === 'topic_created') {
-    const impactType = body.topic.tags.includes(HIGH_IMPACT) ? 'High' : 'Medium'
-
-    await axios.post(
-      DISCOURSE_URL + '/posts.json',
-      {
-        topic_id: body.topic.id,
-        raw: `Attention @impact-alerts this proposal has been given a ${impactType} Impact tag, please consider reviewing it.\n\nIf you would like to sign up for impact alerts, please follow these [instructions](https://forum.makerdao.com)`,
-      },
-      {
-        headers: {
-          'Api-Key': DISCOURSE_API_KEY,
-          'Api-Username': DISCOURSE_API_USERNAME,
-        },
-      }
-    )
-
-    res.status(200).end()
-    return
-  }
-
-  if (eventType === 'topic_edited') {
-    const topicRes = await axios.get(DISCOURSE_URL + `/t/${body.topic.id}.json`)
+  else if (eventType === 'topic_created')
+    await postReply(body.topic.id, impactType)
+  else if (eventType === 'topic_edited') {
+    const topicRes = await forumAxios.get(`/t/${body.topic.id}.json`)
     const topicData = topicRes.data
-    const firstPostId = topicData.post_stream.posts[0].id
+    const latestsBotReply = topicData.post_stream.posts
+      .reverse()
+      .find((post: any) => post.username === DISCOURSE_API_USERNAME)
 
-    const changelogRes = await axios.get(
-      DISCOURSE_URL + `/posts/${firstPostId}/revisions/latest.json`
-    )
-    const changelogData = changelogRes.data
-    const { previous: previousTags, current: currentTags } =
-      changelogData.tags_changes
-    const impactTagsBefore = previousTags.some(
-      (tag: string) => tag === HIGH_IMPACT || tag === MEDIUM_IMPACT
-    )
-    const impactTagsNow = currentTags.some(
-      (tag: string) => tag === HIGH_IMPACT || tag === MEDIUM_IMPACT
-    )
+    if (!latestsBotReply) await postReply(body.topic.id, impactType)
+    else {
+      const replyImpactType = latestsBotReply.cooked.includes('High Impact tag')
+        ? 'High'
+        : 'Medium'
 
-    if (!impactTagsBefore && impactTagsNow) {
-      const impactType = currentTags.includes(HIGH_IMPACT) ? 'High' : 'Medium'
+      if (impactType !== replyImpactType) {
+        if (impactType === 'Medium')
+          await editReply(latestsBotReply.id, impactType)
+        else {
+          const timeDiff =
+            Date.now() - new Date(latestsBotReply.updated_at).getTime()
 
-      await axios.post(
-        DISCOURSE_URL + '/posts.json',
-        {
-          topic_id: body.topic.id,
-          raw: `Attention @impact-alerts this proposal has been given a ${impactType} Impact tag, please consider reviewing it.\n\nIf you would like to sign up for impact alerts, please follow these [instructions](https://forum.makerdao.com)`,
-        },
-        {
-          headers: {
-            'Api-Key': DISCOURSE_API_KEY,
-            'Api-Username': DISCOURSE_API_USERNAME,
-          },
+          if (timeDiff < 1000 * 60 * 60)
+            await editReply(latestsBotReply.id, impactType)
+          else await postReply(body.topic.id, impactType)
         }
-      )
-
-      // Push topic ID to DB - I think a better solution would be to check if the topic has already been replied by the bot
-      // Add a small delay and then fetch latest state for the topic, so people have time to fix mistakes
-      // Maybe instead of a delay we can fetch the last 2 topic edits and compare the time interval between them
-
-      // -- 1 hour delay --
+      }
     }
-
-    res.status(200).end()
-    return
   }
 
   res.status(200).end()
 })
+
+const postReply = async (topic_id: number, impactType: string) =>
+  await forumAxios?.post('/posts.json', {
+    topic_id,
+    raw: `Attention @impact-alerts this proposal has been given a ${impactType} Impact tag, please consider reviewing it.\n\nIf you would like to sign up for impact alerts, please follow these [instructions](https://forum.makerdao.com)`,
+  })
+
+const editReply = async (post_id: number, impactType: string) =>
+  await forumAxios?.put(`/posts/${post_id}.json`, {
+    edit_reason: 'Impact tag changed',
+    raw: `Attention @impact-alerts this proposal has been given a ${impactType} Impact tag, please consider reviewing it.\n\nIf you would like to sign up for impact alerts, please follow these [instructions](https://forum.makerdao.com)`,
+  })
 
 app.listen(port, () =>
   console.log(`⚡️[server]: Server is running on port ${port}...`)
